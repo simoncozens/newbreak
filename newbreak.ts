6edd56248611dc8a8e3ca9ebb3ee123681ba669e
Simon Cozens <simon@simon-cozens.org>
2020-01-16 10:07:58 +0000

    Micro-optimization. Finding cartesian sets is the biggest time killer for this algorithm.

--- newbreak.ts
+++ newbreak.ts
@@ -472,7 +472,7 @@ export class Linebreaker {
     const max = arg.length-1;
     let helper = (arr, i) => {
       for (let j=0, l=arg[i].length; j<l; j++) {
-        const a = [...arr];
+        const a = arr.slice(0);
         a.push(arg[i][j]);
         if (i==max)
             r.push(a);
